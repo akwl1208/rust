1b42211fbdf511e95bad51c47d51efe1741e3557
inyo <pageup64@naver.com>
2026-06-02 16:20:02 +0900

    Stop tracking target/

--- gradient_descent/target/debug/deps/gradient_descent-8a8462d65e4178c0.d
+++
@@ -1,5 +0,0 @@
-/mnt/rust/projects/gradient_descent/target/debug/deps/gradient_descent-8a8462d65e4178c0.d: src/main.rs
-
-/mnt/rust/projects/gradient_descent/target/debug/deps/gradient_descent-8a8462d65e4178c0: src/main.rs
-
-src/main.rs:
--- gradient_descent/target/debug/gradient_descent.d
+++
@@ -1,1 +0,0 @@
-/mnt/rust/projects/gradient_descent/target/debug/gradient_descent: /mnt/rust/projects/gradient_descent/src/main.rs
