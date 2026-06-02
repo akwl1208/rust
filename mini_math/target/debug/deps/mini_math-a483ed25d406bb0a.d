2bf77cff57c856caaf61886e58ad6e42ad689a3d
inyo <pageup64@naver.com>
2026-06-02 16:20:18 +0900

    Stop tracking target/

--- mini_math/target/debug/deps/mini_math-a483ed25d406bb0a.d
+++
@@ -1,5 +0,0 @@
-/mnt/rust/projects/mini_math/target/debug/deps/mini_math-a483ed25d406bb0a.d: src/main.rs
-
-/mnt/rust/projects/mini_math/target/debug/deps/mini_math-a483ed25d406bb0a: src/main.rs
-
-src/main.rs:
--- mini_math/target/debug/mini_math.d
+++
@@ -1,1 +0,0 @@
-/mnt/rust/projects/mini_math/target/debug/mini_math: /mnt/rust/projects/mini_math/src/main.rs
